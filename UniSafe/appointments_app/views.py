e3dc73f9f182f0090f933bc891784786a769da4d
NucciNello <caljr9301@gmail.com>
2024-05-06 01:50:33 +0300

    1. Create Appointments - DONE 2. Accept Appointments - DONE

--- UniSafe/appointments_app/views.py
+++ UniSafe/appointments_app/views.py
@@ -1,3 +1,66 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import *
+from .serializers import *
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
-# Create your views here.
+
+class CreateAppointmentView(generics.CreateAPIView):
+    serializer_class = CreateAppointmentSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def create(self, request, *args, **kwargs):
+        user = request.user
+        if not user.is_student:
+            return Response(
+                {"error": "Only students can create appointments"}, status=403
+            )
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(client=user.profile)
+
+        return Response(serializer.data, status=201)
+
+
+class AcceptAppointmentView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request, *args, **kwargs):
+        user = request.user
+        if not user.is_consultant:
+            return Response(
+                {"error": "Only consultants can accept appointments"},
+                status=status.HTTP_403_FORBIDDEN,
+            )
+
+        # Get the appointment_id from URL kwargs
+        appointment_id = self.kwargs.get("pk")
+
+        try:
+            appointment = Appointment.objects.get(pk=appointment_id)
+        except Appointment.DoesNotExist:
+            return Response(
+                {"error": f"Appointment {appointment_id} doesn't exist."},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+
+        if appointment.status == "SCHEDULED":
+            return Response(
+                {"error": f"Appointment {appointment_id} has already been accepted."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        serializer = AcceptAppointmentSerializer(
+            instance=appointment, data=request.data
+        )
+        serializer.is_valid(raise_exception=True)
+
+        appointment = serializer.save(
+            status="SCHEDULED",
+            consultant=user.profile,
+            consultant_phone=user.phone_number,
+            consultant_office=user.profile.office,
+        )
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
--- UniSafe/main_app/urls.py
+++ UniSafe/main_app/urls.py
@@ -13,24 +13,25 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Snippets API",
-      default_version='v1',
-      description="Test description",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@snippets.local"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
+        title="Snippets API",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
 )
 
 urlpatterns = [
-    path('swagger-download/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path("admin/", admin.site.urls),
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    path("users/", include("users_app.urls")),
+    path('appointments/', include('appointments_app.urls')),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('reports/', include('reports_app.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('swagger-download/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path("users/", include("users_app.urls")),
 ]
