edcc2aebb6d2293c8e76ef72d6e4190e50ea51ba
NucciNello <caljr9301@gmail.com>
2024-06-20 14:19:15 +0300

    "Added media settings and URL configuration for serving media files"

--- UniSafe/main_app/urls.py
+++ UniSafe/main_app/urls.py
@@ -8,13 +8,15 @@ from rest_framework import permissions
 
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.conf.urls.static import static
+from django.conf import settings
 
 ...
 
 schema_view = get_schema_view(
-   openapi.Info(
+    openapi.Info(
         title="Snippets API",
-        default_version='v1',
+        default_version="v1",
         description="Test description",
         terms_of_service="https://www.google.com/policies/terms/",
         contact=openapi.Contact(email="contact@snippets.local"),
@@ -28,11 +30,17 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    path('appointments/', include('appointments_app.urls')),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    path('reports/', include('reports_app.urls')),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('swagger-download/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('statistics/', include('statistics_app.urls')),
+    path("appointments/", include("appointments_app.urls")),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+    path("reports/", include("reports_app.urls")),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path(
+        "swagger-download/", schema_view.without_ui(cache_timeout=0), name="schema-json"
+    ),
+    path("statistics/", include("statistics_app.urls")),
     path("users/", include("users_app.urls")),
-]
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
