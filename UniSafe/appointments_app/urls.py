f57e3155700c6b21e2aecf6c22373e00eff024a8
NucciNello <caljr9301@gmail.com>
2024-06-30 11:19:49 +0300

    "Updated appointment URLs and views, and modified database files."

--- UniSafe/appointments_app/urls.py
+++ UniSafe/appointments_app/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path("create", CreateAppointmentView.as_view(), name="create-appointment"),
     path('accept/<str:pk>/', AcceptAppointmentView.as_view(), name='accept_appointment'),
     path('list/requested', StudentAppointmentsListView.as_view(), name='student_appointments'),
-    path('cancel/<int:pk>/', CancelAppointmentView.as_view(), name='cancel_appointment'),
+    path('cancel/<str:pk>/', CancelAppointmentView.as_view(), name='cancel_appointment'),
 ]
